8b0692e95fb010ef4ad77033b4bff5abb22c7132
David Morasz <david.morasz@meso.design>
2019-06-27 22:10:16 +0200

    separating BlueWindow source code to its submodule

--- Plugins/BlueWindow/Source/BlueWindow/Private/BlueWindowSettings.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BlueWindowSettings.h"
-
-// Add default functionality here for any IBlueWindowSettings functions that are not pure virtual.
